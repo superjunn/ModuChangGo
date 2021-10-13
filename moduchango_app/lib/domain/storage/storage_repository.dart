84cb76cf3efc0e7a4ec5394fbf7e39a0addf48f9
TaehanKim00 <tanit23@kaist.ac.kr>
2021-10-13 22:28:04 +0900

    server communication building

--- moduchango_app/lib/domain/storage/storage_repository.dart
+++ moduchango_app/lib/domain/storage/storage_repository.dart
@@ -5,16 +5,17 @@ import 'package:moduchango_app/domain/storage/storage_provider.dart';
 class StorageRepository {
   final StorageProvider _storageProvider = StorageProvider();
 
-  Future<void> findAll() async {
+  Future<List<Storage>> findAll() async {
     Response response = await _storageProvider.findAll();
     dynamic body = response.body;
     if (body.runtimeType == List) {
       List<dynamic> temp = body;
       List<Storage> storages = temp.map((e) => Storage.fromJson(e)).toList();
-      print(storages.length);
-      print(storages[0].storageName);
+      //print(storages.length)
+      //print(storages[0].strageName)
+      return storages;
     } else {
-      print(0);
+      return <Storage>[];
     }
   }
 }
