fd13c313fdcbba5b5ff33d5ddfded95c632b4d03
superjunn <87594550+superjunn@users.noreply.github.com>
2021-10-13 12:41:28 +0000

    de

--- moduchango_web/lib/domain/storage/storage_repository.dart
+++ moduchango_web/lib/domain/storage/storage_repository.dart
@@ -1,13 +1,14 @@
+import 'package:get/get.dart';
 import 'package:get/get_connect.dart';
 import 'package:moduchango/domain/storage/storage.dart';
 import 'package:moduchango/domain/storage/storage_provider.dart';
 
 //통신을 호출해서 응답되는 데이터를 예쁘게 가공 => json => Dart 오브젝트
 class StorageRepository {
-  final StorageProvider _storageProvider = StorageProvider();
+  StorageProvider s = Get.put(StorageProvider());
 
   Future<List<Storage>> view() async {
-    Response response = await _storageProvider.view();
+    Response response = await s.view();
     dynamic body = response.body;
     print("body : ${body}");
     List<Storage> storages =
