1a53beded5cd1f5c9194866dde720442c1da5c86
superjunn <87594550+superjunn@users.noreply.github.com>
2021-10-17 10:58:44 +0000

    server connecting

--- WEB(FE)/lib/controller/storage_controller.dart
+++ WEB(FE)/lib/controller/storage_controller.dart
@@ -1,10 +1,25 @@
 import 'package:get/get.dart';
+import 'package:moduchango/domain/storage/storage.dart';
 import 'package:moduchango/domain/storage/storage_repository.dart';
 
 class StorageController extends GetxController {
   final StorageRepository _storageRepository = StorageRepository();
-  Future<String> findAll(){
-    Future<String> ret = _storageRepository.findAll();
-    return ret;
+  final storages = <Storage>[].obs;
+  final storage = Storage().obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    findAll();
   }
+
+  Future<void> findAll() async {
+    List<Storage> storages = await _storageRepository.findAll();
+    this.storages.value = storages;
+  }
+
+  // Future<void> findByName(String name) async {
+  //   Storage storage = await _storageRepository.findById(name);
+  //   this.storage.value = storage;
+  // }
 }
--- WEB(FE)/lib/domain/storage/storage_repository.dart
+++ WEB(FE)/lib/domain/storage/storage_repository.dart
@@ -1,13 +1,17 @@
 import 'package:get/get.dart';
+import 'package:moduchango/domain/storage/storage.dart';
 import 'package:moduchango/domain/storage/storage_provider.dart';
 
 class StorageRepository {
   final StorageProvider _storageProvider = StorageProvider();
 
-  Future<String> findAll() async {
+  Future<List<Storage>> findAll() async {
     Response response = await _storageProvider.findAll();
     dynamic body = response.body;
     print("body : ${body}");
-    return body;
+
+    List<Storage> storages =
+        body.map((storage) => Storage.fromJson(storage)).toList();
+    return storages;
   }
 }
