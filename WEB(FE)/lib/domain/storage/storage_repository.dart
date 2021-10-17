60db0b92e9a979d69fbba82361bc9df32bfdabd5
superjunn <superjunn00@kaist.ac.kr>
2021-10-17 22:46:28 +0900

    server connecting

--- WEB(FE)/lib/domain/storage/storage_repository.dart
+++ WEB(FE)/lib/domain/storage/storage_repository.dart
@@ -8,14 +8,14 @@ class StorageRepository {
   Future<List<Storage>> findAll() async {
     Response response = await _storageProvider.findAll();
     dynamic body = response.body;
-    print("body : ${body}");
+    print("레포지토리가 받아온 body는 ?? : ${body}");
 
     if (body.runtimeType == List) {
       List<dynamic> temp = body;
       List<Storage> storages = temp.map((e) => Storage.fromJson(e)).toList();
       return storages;
     } else {
-      print("여기서 이게 출력되는구만 왜 못 받는 거야 띠 뽜 !!!");
+      print("런타임타입이 리스트가 아니어서 디폴트 빈리스트 리턴 in REpository !!");
       return <Storage>[];
     }
   }
