126cc6fbfa9692db50591eeded6f5a9878276cdd
박규한 <rbgks1937@naver.com>
2021-10-17 09:58:01 +0000

    delete error update

--- WEB(BE)/app.js
+++ WEB(BE)/app.js
@@ -14,6 +14,7 @@ const historyRouter = require('./routes/history');
 
 app.use(logger('dev'));
 app.use(cookieParser());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
