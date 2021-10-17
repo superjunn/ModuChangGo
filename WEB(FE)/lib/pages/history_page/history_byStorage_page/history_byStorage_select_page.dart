d2b13da3d30fec6c8ce97ea6027da96f5537c2b7
superjunn <87594550+superjunn@users.noreply.github.com>
2021-10-17 14:30:06 +0000

    Storage view server connected

--- WEB(FE)/lib/pages/history_page/history_byStorage_page/history_byStorage_select_page.dart
+++ WEB(FE)/lib/pages/history_page/history_byStorage_page/history_byStorage_select_page.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:moduchango/components/common/page_footer.dart';
 import 'package:moduchango/components/common/page_header.dart';
 import 'package:moduchango/components/common/storage_form.dart';
+import 'package:moduchango/controller/storage_controller.dart';
 import 'package:moduchango/design_data/size.dart';
 import 'package:moduchango/pages/history_page/history_page_components/history_page_left_menu_bar.dart';
 
 class HistoryByStorageSelectPage extends StatelessWidget {
+  var refreshKey = GlobalKey<RefreshIndicatorState>();
+
   @override
   Widget build(BuildContext context) {
+    StorageController s = Get.find();
+
     double bodyWidth = getBodyWidth(context);
     double currentSize = MediaQuery.of(context).size.width;
 
@@ -28,17 +34,36 @@ class HistoryByStorageSelectPage extends StatelessWidget {
   }
 
   _buildBody() {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        HistoryPageLeftMenuBar(),
-        Expanded(
-          child: Wrap(
-            spacing: 10,
-            children: List.generate(50, (index) => StorageForm(formWidth: storage_edge_b, stat: Status.history)),
-          ),
+    StorageController s = Get.find();
+
+    return Obx(
+      () => RefreshIndicator(
+        key: refreshKey,
+        onRefresh: () async {
+          await s.findAll();
+        },
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            HistoryPageLeftMenuBar(),
+            Expanded(
+              child: Wrap(
+                spacing: 10,
+                children: List.generate(
+                  s.storages.length,
+                  (index) => StorageForm(
+                    storageName: s.storages[index].storageName!,
+                    location: s.storages[index].location!,
+                    image_id: s.storages[index].image!,
+                    formWidth: storage_edge_b,
+                    stat: Status.history,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
--- WEB(FE)/lib/pages/storehouse_page/storehouse_view/storehouse_my_storages_view_page.dart
+++ WEB(FE)/lib/pages/storehouse_page/storehouse_view/storehouse_my_storages_view_page.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:moduchango/components/common/page_footer.dart';
 import 'package:moduchango/components/common/page_header.dart';
 import 'package:moduchango/components/common/storage_form.dart';
+import 'package:moduchango/controller/storage_controller.dart';
 import 'package:moduchango/design_data/size.dart';
 import 'package:moduchango/design_data/styles.dart';
 import 'package:moduchango/pages/storehouse_page/storehouse_page_components/storehouse_page_left_menu_bar.dart';
 
 class StoreHouseMyStoragesViewPage extends StatelessWidget {
+  var refreshKey = GlobalKey<RefreshIndicatorState>();
+
   @override
   Widget build(BuildContext context) {
     double bodyWidth = getBodyWidth(context);
@@ -40,18 +44,35 @@ class StoreHouseMyStoragesViewPage extends StatelessWidget {
   }
 
   _buildBody() {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        StoreHousePageLeftMenuBar(),
-        Expanded(
-          child: Wrap(
-            spacing: 10,
-            children: List.generate(
-                50, (index) => StorageForm(formWidth: storage_edge_b + 20)),
-          ),
+    StorageController s = Get.find();
+
+    return Obx(
+      () => RefreshIndicator(
+        key: refreshKey,
+        onRefresh: () async {
+          await s.findAll();
+        },
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            StoreHousePageLeftMenuBar(),
+            Expanded(
+              child: Wrap(
+                spacing: 10,
+                children: List.generate(
+                  s.storages.length,
+                  (index) => StorageForm(
+                    storageName: s.storages[index].storageName!,
+                    location: s.storages[index].location!,
+                    image_id: s.storages[index].image!,
+                    formWidth: storage_edge_b + 20,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
