d18f0f6da665773ed1124dd472ce0b2b955e1367
superjunn <superjunn00@kaist.ac.kr>
2021-10-18 12:42:07 +0900

    server reconnecting

--- WEB(FE)/lib/controller/storage_controller.dart
+++ WEB(FE)/lib/controller/storage_controller.dart
@@ -1,9 +1,31 @@
 import 'package:get/get.dart';
+import 'package:moduchango/domain/content/content.dart';
+import 'package:moduchango/domain/storage/storage.dart';
 import 'package:moduchango/domain/storage/storage_repository.dart';
 
 class StorageController extends GetxController {
   final StorageRepository _storageRepository = StorageRepository();
-  void findAll() {
-    _storageRepository.findAll();
+  final storages = <Storage>[].obs;
+  final storage = <Content>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    findAll();
   }
+
+  Future<void> findAll() async {
+    List<Storage> storages = await _storageRepository.findAll();
+    this.storages.value = storages;
+  }
+
+  Future<void> findByID(String storageName) async {
+    List<Content> storage = await _storageRepository.findByName(storageName);
+    this.storage.value = storage;
+  }
+
+// Future<void> findByName(String name) async {
+//   Storage storage = await _storageRepository.findById(name);
+//   this.storage.value = storage;
+// }
 }
--- WEB(FE)/lib/domain/storage/storage_repository.dart
+++ WEB(FE)/lib/domain/storage/storage_repository.dart
@@ -1,12 +1,36 @@
 import 'package:get/get.dart';
+import 'package:moduchango/domain/content/content.dart';
+import 'package:moduchango/domain/storage/storage.dart';
 import 'package:moduchango/domain/storage/storage_provider.dart';
 
 class StorageRepository {
   final StorageProvider _storageProvider = StorageProvider();
 
-  Future<void> findAll() async {
+  Future<List<Storage>> findAll() async {
     Response response = await _storageProvider.findAll();
     dynamic body = response.body;
-    print("body : ${body}");
+    print("레포지토리가 받아온 body는 ?? : ${body}");
+
+    if (body.runtimeType == List) {
+      List<dynamic> temp = body;
+      List<Storage> storages = temp.map((e) => Storage.fromJson(e)).toList();
+      return storages;
+    } else {
+      print("런타임타입이 리스트가 아니어서 디폴트 빈리스트 리턴 in REpository !!");
+      return <Storage>[];
+    }
+  }
+
+  Future<List<Content>> findByName(String storageName) async {
+    Response response = await _storageProvider.findByName(storageName);
+    dynamic body = response.body;
+    if (body.runtimeType == List) {
+      List<dynamic> temp = body;
+      List<Content> contents = temp.map((e) => Content.fromJson(e)).toList();
+      return contents;
+    } else {
+      print("런타임타입이 리스트가 아니어서 디폴트 빈리스트 리턴 in Repository findByName !!");
+      return <Content>[];
+    }
   }
 }
