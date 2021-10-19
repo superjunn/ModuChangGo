46390c3666b52ada4fe96fea94020f7db05e22ea
TaehanKim00 <tanit23@kaist.ac.kr>
2021-10-19 18:23:42 +0900

    history developing

--- APP/lib/view/pages/stock_release_history/stock_release_history.dart
+++ APP/lib/view/pages/stock_release_history/stock_release_history.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:moduchango_app/controller/history_controller.dart';
-import 'package:moduchango_app/view/pages/stock_release_history/custom_history_tile.dart';
+import 'package:moduchango_app/view/pages/stock_release_history/history_components/custom_history_tile.dart';
 
 class StockReleaseHistory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    HistoryController h = Get.find();
+    HistoryController h = Get.put(HistoryController());
 
     return Obx(() => Column(
           children: List.generate(h.history.length, (i) {
-            return CustomHistoryTile();
+            return CustomHistoryTile(
+              id: h.history[i].id,
+              NIIN: h.history[i].NIIN,
+              productName: h.history[i].productName,
+              state: h.history[i].state,
+              storageName: h.history[i].storageName,
+              time: h.history[i].time,
+            );
           }),
         ));
   }
